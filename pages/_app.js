22baaf25907fe28705766227cd250e3a693f8b80
Sergio Luiz Paganini <slp7711@gmail.com>
2022-10-20 14:01:07 -0300

    Change format of app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
-import Layout from '../components/Layout'
+import Footer from '../components/Footer'
+//import Layout from '../components/Layout'
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+/* function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
+  )
+} */
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className="content">
+      <Navbar/>
+      <Component {...pageProps} />
+      <Footer/>
+    </div>
   )
 }
 
