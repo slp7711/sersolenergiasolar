b5e69fd3f960617297735f56d6bd1d885bd2000b
Sergio Luiz Paganini <slp7711@gmail.com>
2022-11-17 21:21:33 -0300

    correção environment variable google atg

--- components/Google-tag.js
+++ components/Google-tag.js
@@ -10,7 +10,7 @@ const GoogleTag = () => {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer',process.env.GOOGLE_TAG);`}
+})(window,document,'script','dataLayer',${process.env.GOOGLE_TAG});`}
                 </Script>
             </Head>
             <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.GOOGLE_TAG}`}
