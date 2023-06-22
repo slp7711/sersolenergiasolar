fd08679c0f4e0815d931d6d81c3d640565793de9
Sergio Luiz Paganini <slp7711@gmail.com>
2023-06-22 14:35:14 -0300

    Alteração caracteriscas kit energia solar

--- pages/kitEnergiaSolar.js
+++ pages/kitEnergiaSolar.js
@@ -46,14 +46,14 @@ const KitEnergiaSolar = () => {
                             <Image src="/kit.jpg" width={400} height={300} alt="imagem kit de energia solar"/>
                         </div>
                         <div className={styles.kit_resumo}>
-                            <p>Potência: 2,38Kwp</p>
-                            <p>Energia produzida: até 280 Kh/mês</p>
-                            <p>Paineis: 4 x 595 Wp Luxen</p>
+                            <p>Potência: 2,24Kwp</p>
+                            <p>Energia produzida: até 225 Kh/mês</p>
+                            <p>Paineis: 4 x 560 Wp Luxen</p>
                             <p>Microinversor: 1 x SUN2000 Deye</p>
                             <p>Estrutura de fixação: inclusa -  de acordo com o tipo de telhado</p>
                             <p>Projeto:  incluso - elaboração e aprovação junto a concessionária</p>
                             <p>Instalação: Kit instalado e liberado para conecção a rede da concessionária</p>
-                            <p>Promoção especial: R$ 9.980,00</p>
+                            <p>Promoção especial: R$ 6.990,00</p>
                             <p>Sua usina funcionando em 30 dias</p>
                             <p>Entre em contato conosco agora mesmo e garanta a sua usina solar com as melhores condições do mercado.</p>
                         </div>
